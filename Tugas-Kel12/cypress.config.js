946c04d4b1db046f882f487c8798aae3bc8297e4
Maulana Syaeikhi Naefiq <maulana.snaefiq08@gmail.com>
2025-01-19 23:40:57 +0700

    update form main

---
+++ Tugas-Kel12/cypress.config.js
@@ -0,0 +1,12 @@
+const { defineConfig } = require("cypress");
+
+module.exports = defineConfig({
+  e2e: {
+    baseUrl : 'https://magento.softwaretestingboard.com/',
+    viewportWidth :900,
+    viewportHeight : 1000,
+    setupNodeEvents(on, config) {
+      // implement node event listeners here
+    },
+  },
+});
