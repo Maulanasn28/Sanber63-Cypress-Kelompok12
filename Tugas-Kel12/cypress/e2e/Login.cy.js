ac4a9f2f5198512b16582458a82717e3c9e0122f
Rila Afhrila <rilaafhrila15@gmail.com>
2025-01-20 09:59:42 +0700

    merge login scaenario

--- Tugas-Kel12/cypress/e2e/Login.cy.js
+++ Tugas-Kel12/cypress/e2e/Login.cy.js
@@ -1,7 +1,12 @@
+import homepage from "../support/pageObject/homepage";
+import signIn from "../support/pageObject/signIn";
+
 describe('Registration', () => {
     beforeEach(() => {
       cy.visit('')
+      cy.verifyTittlePage('.base', 'Home Page')
     })
+
     it('Login Success', () => {
         cy.visit('https://magento.softwaretestingboard.com/customer/account/login/referer/aHR0cHM6Ly9tYWdlbnRvLnNvZnR3YXJldGVzdGluZ2JvYXJkLmNvbS8%2C/Sign In')
         cy.get ('#email').type ('SayaPelajar12345@gmail.com')
@@ -9,4 +14,34 @@ describe('Registration', () => {
         cy.get ('#send2').click()
 
       })
+
+    it('Success Sign In with valid account', () => {
+            cy.get('.panel > .header > .authorization-link > a').click()
+            cy.verifyTittlePage('.base', 'Customer Login')
+            cy.get(signIn.email).click().type('qa999magento@mail.com')
+            cy.get(signIn.password).click().type('Magento_SoftwareTestingBoard')
+            cy.get('.login-container > .block-customer-login > .block-content > #login-form > .fieldset > .actions-toolbar > div.primary > #send2 > span').click()
+            cy.wait(500)
+            cy.verifySuccess(':nth-child(2) > .greet > .logged-in', 'Welcome, QA')
+          })
+    
+          it('Success Sign In with valid account (fixtures)', () => {
+            cy.fixture('usersLogin.json').then((usersLogin) => {
+              if (usersLogin.length === 0) {
+                throw new Error('usersLogin.json is empty.');
+              }
+              const datauser0 = usersLogin[0];
+              cy.signInOption(datauser0.email, datauser0.password);
+            });
+           })
+      
+           it('Sign In with invalid account (fixtures)', () => {
+            cy.fixture('usersLogin.json').then((usersLogin) => {
+              if (usersLogin.length === 0) {
+                throw new Error('usersLogin.json is empty.');
+              }
+              const datauser1 = usersLogin[1];
+              cy.signInInValid(datauser1.email, datauser1.password);
+            });
+           })
 })
